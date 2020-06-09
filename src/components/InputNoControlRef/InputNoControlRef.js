3aa7c8d6488b5f8c3d2cd359940c01468a85b2e7
Didier Sarmiento <irvingdidier.sarmiento@gmail.com>
2020-06-09 14:16:44 -0500

    add for attribute to label

--- src/components/InputNoControlRef/InputNoControlRef.js
+++ src/components/InputNoControlRef/InputNoControlRef.js
@@ -13,9 +13,34 @@ class InputNoControl extends React.Component {
   render() { 
     return (
       <form onSubmit={this.handledSubmit}>
-        <input className='input' placeholder='Nombre' type="text" name="name"/>
-        <input className='input' placeholder='Correo electrónico' type="text" name="email"/>
-        <button className="button">Enviar</button>
+        <div className="field is-horizontal">
+          <div className="field-label is-normal">
+            <label className="label" htmlFor='name'>Nombre</label>
+          </div>
+          <div className="field-body">
+            <div className="field">
+              <p className="control">
+                <input className='input' placeholder='Nombre' type="text" name="name" id="name"/>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="field is-horizontal">
+          <div className="field-label is-normal">
+            <label className="label" htmlFor='email'>Correo electrónico</label>
+          </div>
+          <div className="field-body">
+            <div className="field">
+              <p className="control">
+                <input className='input' placeholder='Correo electrónico' type="text" name="email" id="email"/>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="buttons is-right">
+          <button className="button is-link">Enviar</button>
+        </div>
       </form>
     )
   }
--- src/components/Refs/Refs.js
+++ src/components/Refs/Refs.js
@@ -23,8 +23,10 @@ export default class Refs extends React.Component {
         <div className='card-content'>
           <p className='title'>Usando las refs con React</p>
           <input className="input" type="text" ref={this.entry}></input>
-          <button className="button" onClick={this.focus}>Focus</button>
-          <button className="button" onClick={this.blur}>Blur</button>
+          <div className="buttons is-right">
+            <button className="button is-focused" onClick={this.focus}>Focus</button>
+            <button className="button" onClick={this.blur}>Blur</button>
+          </div>
         </div>
       </div>
     )
