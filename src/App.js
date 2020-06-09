0e73bde2abc2e6efdf5b4204f42c320fd5adf6b3
Didier Sarmiento <irvingdidier.sarmiento@gmail.com>
2020-06-08 22:52:53 -0500

    using refs to manage dom

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import PropTypes from './components/ValidPropTypes';
 import SectionComponent from './components/common/SectionComponent';
 import NavComponent from './components/common/NavComponent';
 import IterateLists from './components/IterateLists';
+import Refs from './components/Refs';
 
 function App() {
 
@@ -53,6 +54,7 @@ function App() {
       <SectionComponent type='info' title='Iterando listas' subtitle='Como recorrer arrays, objetos, componentes y/o listas' medium bold/>
       <div className='container'>
         <IterateLists/>
+        <Refs></Refs>
       </div>
     </div>
   );
---
+++ src/components/Refs/Refs.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export default class Refs extends React.Component {
+
+  entry = React.createRef();
+
+  componentDidMount() {
+    this.focus()
+  }
+
+  focus = () => {
+    console.log('ref entry', this.entry)
+    this.entry.current.focus()
+  }
+  
+  blur = () => {
+    this.entry.current.blur()
+  }
+
+  render() {
+    return (
+      <div className='card'>
+        <div className='card-content'>
+          <p className='title'>Usando las refs con React</p>
+          <input className="input" type="text" ref={this.entry}></input>
+          <button className="button" onClick={this.focus}>Focus</button>
+          <button className="button" onClick={this.blur}>Blur</button>
+        </div>
+      </div>
+    )
+  }
+}
